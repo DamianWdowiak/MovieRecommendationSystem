82b1356c533bc0a6eb572fed47bef45992529fd4
Wiktoura <wiktoria.rozanska1@gmail.com>
2020-10-27 09:43:16 +0100

    reformat

---
+++ emotional_based_filter.py
@@ -0,0 +1,22 @@
+import pickle
+
+from popularity_based_filter import weighted_rating
+
+data = pickle.load(open("datasets/title.merged.sav", "rb"))
+
+emotions = {
+    "Scary": "Horror|Thriller",
+    "Happy": "Music|Musical|Animation|Comedy",
+    "Sad": "Documentary|War|History|Drama|Reality-TV",
+    "Melting mood": "Biography|Romance",
+    "Action craving": "Western|Sport|Adventure|Action|Sci-Fi|Fantasy",
+    "Mysterious": "Mystery|Crime|Film-Noir|Adult",
+    "Curious": "News|Talk-Show",
+    "Light": "Game-Show|Family|Short"
+}
+
+def recomendedFilmToYourMood(emotion,n):
+    e_data = data.copy().loc[data['genres'].str.contains(emotions[emotion])]
+    e_data['score'] = e_data.apply(weighted_rating, axis=1)
+    e_data = e_data.sort_values('score', ascending=False)
+    return e_data[['primaryTitle','genres','averageRating']].head(n)
